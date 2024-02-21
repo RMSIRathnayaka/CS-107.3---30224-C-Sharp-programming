ac657533832bcb6d7bc06f5cda6616755e297b86
RMSIRathnayaka <135612054+RMSIRathnayaka@users.noreply.github.com>
2024-02-21 11:53:32 +0530

    Add files via upload

---
+++ labsheet 1/lab1/lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Enter number #{i + 1}: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"{number} is {(number % 2 == 0 ? "even" : "odd")}");
+            }
+
+
+        }
+    }
+}
